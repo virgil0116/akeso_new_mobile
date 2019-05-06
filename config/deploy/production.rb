c6d3cc028691bb9b3f4c1afd32993c08d0bd199f
WJ <jsj_wj@163.com>
2019-05-06 17:00:24 +0800

    fix add deploy

---
+++ config/deploy/production.rb
@@ -0,0 +1,4 @@
+set :domain, 'ubuntu@47.93.114.49'
+set :deploy_to, '/www/akeso_new_mobile'
+set :repository, 'git@github.com:virgil0116/akeso_new_mobile.git'
+set :branch, 'master'
---
+++ config/deploy/staging.rb
@@ -0,0 +1,4 @@
+set :domain, 'www@47.93.78.171'
+set :deploy_to, '/var/www/akeso_new_mobile'
+set :repository, 'git@github.com:virgil0116/akeso_new_mobile.git'
+set :branch, 'staging'
