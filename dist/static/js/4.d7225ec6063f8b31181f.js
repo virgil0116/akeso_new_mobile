003a5b7ed7d6efe48a5c178fc65066df46b6a708
WJ <jsj_wj@163.com>
2019-05-10 15:05:24 +0800

    npm run build

--- dist/static/js/4.d7225ec6063f8b31181f.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([4],{"2Lz7":function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var s={render:function(){this.$createElement;this._self._c;return this._m(0)},staticRenderFns:[function(){var e=this.$createElement,t=this._self._c||e;return t("div",{staticClass:"container"},[t("p",[this._v("bind_success")])])}]};var c=n("VU/8")({name:"BindSuccess"},s,!1,function(e){n("LJVe")},"data-v-0356123a",null);t.default=c.exports},LJVe:function(e,t){}});
-//# sourceMappingURL=4.d7225ec6063f8b31181f.js.map
